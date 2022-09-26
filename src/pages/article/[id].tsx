affa3b0eb0b6d4f557ebff4e4286e2b9c1a23f52
Tatsuya1117 <othe.world.pc@gmail.com>
2022-09-26 15:27:40 +0900

    [id].tsxにSEOを追加

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import { client } from "../../libs/client";
+import SEO from "../../components/layouts/SEO";
 import type { Article } from "../../types/article";
 import styles from "../../styles/Stylist/Detail.module.scss";
 
@@ -12,50 +13,59 @@ type Props = {
 
 export default function Article({ article }: Props) {
   return (
-    <div className={styles.detailArea}>
-      <Link href={"/"}>
-        <a>
-          <h2 className={styles.detailMainTitle}>Topics</h2>
-        </a>
-      </Link>
-      <div className={styles.detailMainArea}>
-        <div className="">
-          <div className={styles.detailSubTitle}>{article.title}</div>
-        </div>
-        {article.tag && (
-          <div className="">
-            <div className={styles.detailTag}>#{article.tag}</div>
-          </div>
-        )}
-        <div className={styles.detailStyleImage}>
-          <Image
-            className=""
-            src={article.eye_catch.url}
-            objectFit="contain"
-            alt={article.title + "の画像です"}
-            width={960}
-            height={960}
-          />
-        </div>
-        <div className="">
-          <div className={styles.detailBody1}>
-            {/* Prevent HTML tags from being output */}
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `${article.body}`,
-              }}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.next}>
-        <Link href={"/"} passHref>
-          <a className={styles.nextInner}>
-            <span className={styles.nextInnerIn}>Topに戻る</span>
+    <>
+      <SEO
+        title={article.title + " | " + article.subTitle}
+        description={article.description}
+        keyword={article.keyword}
+        image={article.eye_catch.url}
+        url={`/article/${article.id}`}
+      />
+      <div className={styles.detailArea}>
+        <Link href={"/"}>
+          <a>
+            <h2 className={styles.detailMainTitle}>Topics</h2>
           </a>
         </Link>
+        <div className={styles.detailMainArea}>
+          <div className="">
+            <div className={styles.detailSubTitle}>{article.title}</div>
+          </div>
+          {article.tag && (
+            <div className="">
+              <div className={styles.detailTag}>#{article.tag}</div>
+            </div>
+          )}
+          <div className={styles.detailStyleImage}>
+            <Image
+              className=""
+              src={article.eye_catch.url}
+              objectFit="contain"
+              alt={article.title + "の画像です"}
+              width={960}
+              height={960}
+            />
+          </div>
+          <div className="">
+            <div className={styles.detailBody1}>
+              {/* Prevent HTML tags from being output */}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `${article.body}`,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.next}>
+          <Link href={"/"} passHref>
+            <a className={styles.nextInner}>
+              <span className={styles.nextInnerIn}>Topに戻る</span>
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/types/article.ts
+++ src/types/article.ts
@@ -1,17 +1,21 @@
 export type Article = {
-    id: string;
-    totalCount: number;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    title: string;
-    body: string;
-    eye_catch: {
-      url: string;
-      height: number;
-      width: number;
-    };
-    tag: string;
+  id: string;
+  totalCount: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  subTitle: string;
+  body: string;
+  eye_catch: {
+    url: string;
+    height: number;
+    width: number;
   };
+  tag: string;
+  description: string;
+  keyword: string;
+  url: string;
+};
   
