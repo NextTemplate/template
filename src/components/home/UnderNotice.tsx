3db242ff34a285fea2e21afd2cc781eaa8d456c8
Tatsuya1117 <othe.world.pc@gmail.com>
2022-12-07 22:33:06 +0900

    linterCheck

--- src/components/home/UnderNotice.tsx
+++ src/components/home/UnderNotice.tsx
@@ -37,7 +37,11 @@ export default function UnderNotice() {
                   <li className={styles.innerMain_06}>&nbsp;</li>
                   <li className={styles.innerMain_07}>&nbsp;</li>
                   <li className={styles.innerMain_08}>Gallery</li>
-                  <li className={`${styles.innerMain_08_01} ${styles.korokoro}`}>&nbsp;</li>
+                  <li
+                    className={`${styles.innerMain_08_01} ${styles.korokoro}`}
+                  >
+                    &nbsp;
+                  </li>
                 </ul>
               </button>
             </Link>
