a4f737675bc40ed5adcac91e8d6d02f37db1c1f1
Tatsuya1117 <othe.world.pc@gmail.com>
2022-12-07 10:29:59 +0900

    animeを調整

--- src/components/home/UnderNotice.tsx
+++ src/components/home/UnderNotice.tsx
@@ -13,9 +13,7 @@ export default function UnderNotice() {
               <button className={styles.iconButton}>
                 <ul className={styles.inner}>
                   <li className={styles.innerMain_00}>&nbsp;</li>
-                  <li className={`${styles.innerMain_01} ${styles.puyon}`}>
-                    &nbsp;
-                  </li>
+                  <li className={styles.innerMain_01}>&nbsp;</li>
                   <li className={styles.innerMain_02}>Home</li>
                 </ul>
               </button>
@@ -26,9 +24,7 @@ export default function UnderNotice() {
               <button className={styles.iconButton}>
                 <ul className={styles.inner}>
                   <li className={styles.innerMain_03}>&nbsp;</li>
-                  <li className={`${styles.innerMain_04} ${styles.purupuru}`}>
-                    &nbsp;
-                  </li>
+                  <li className={styles.innerMain_04}>&nbsp;</li>
                   <li className={styles.innerMain_05}>About</li>
                 </ul>
               </button>
@@ -39,9 +35,7 @@ export default function UnderNotice() {
               <button className={styles.iconButton}>
                 <ul className={styles.inner}>
                   <li className={styles.innerMain_06}>&nbsp;</li>
-                  <li className={`${styles.innerMain_07} ${styles.pururun}`}>
-                    &nbsp;
-                  </li>
+                  <li className={styles.innerMain_07}>&nbsp;</li>
                   <li className={styles.innerMain_08}>Gallery</li>
                 </ul>
               </button>
@@ -52,9 +46,7 @@ export default function UnderNotice() {
               <button className={styles.iconButton}>
                 <ul className={styles.inner}>
                   <li className={styles.innerMain_09}>&nbsp;</li>
-                  <li className={`${styles.innerMain_10} ${styles.purun}`}>
-                    &nbsp;
-                  </li>
+                  <li className={styles.innerMain_10}>&nbsp;</li>
                   <li className={styles.innerMain_11}>Vision</li>
                 </ul>
               </button>
@@ -65,9 +57,7 @@ export default function UnderNotice() {
               <button className={styles.iconButton}>
                 <ul className={styles.inner}>
                   <li className={styles.innerMain_12}>&nbsp;</li>
-                  <li className={`${styles.innerMain_13} ${styles.poyooon}`}>
-                    &nbsp;
-                  </li>
+                  <li className={styles.innerMain_13}>&nbsp;</li>
                   <li className={styles.innerMain_14}>Contact</li>
                 </ul>
               </button>
