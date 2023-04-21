2eedb3d76caa1b6c07fae9ab996b76fccd470b97
Tatsuya1117 <othe.world.pc@gmail.com>
2023-04-21 10:38:51 +0900

    Adjust position of prevArrow and nextArrow.

--- src/components/Slick/index.tsx
+++ src/components/Slick/index.tsx
@@ -9,47 +9,85 @@ import NextArrow from "./NextArrow";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const images: string[] = [
-    "/media/mini_00.jpg",
-    "/media/mini_01.jpg",
-    "/media/mini_02.jpg",
-    "/media/mini_03.jpg",
-    "/media/mini_04.jpg",
+  const images = [
+    {
+      src: "/media/mini_00.jpg",
+      link: "/",
+    },
+    {
+      src: "/media/mini_01.jpg",
+      link: "/",
+    },
+    {
+      src: "/media/mini_02.jpg",
+      link: "/",
+    },
+    {
+      src: "/media/mini_03.jpg",
+      link: "/",
+    },
+    {
+      src: "/media/mini_04.jpg",
+      link: "/",
+    },
   ];
 
 const CustomSlick = () => {
   const settings = {
+    autoplay: true,
+    autoplaySpeed: 4000,
     dots: true,
     infinite: true,
     speed: 500,
     centerMode: true,
     centerPadding: '0',
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     // オリジナルArrow
     prevArrow: <PrevArrow onClick={() => onclick} />,
     nextArrow: <NextArrow onClick={() => onclick} />,
+    // ブレイクポイントの指定
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div className={styles.slickBody}>
     <Slider {...settings}>
-      {images.map((img) => {
+      {images.map((imgObj) => {
         return (
-          <div key={img}>
-            <Link
-              href={"/"}
-              passHref
-            >
+          <div key={imgObj.src}>
+            <Link href={imgObj.link} passHref>
               <Image
-                src={img}
+                src={imgObj.src} 
                 height={440}
                 width={440}
                 alt={"alt"}
               />
             </Link>
           </div>
-        )
+        );
       })}
     </Slider>
     </div>
